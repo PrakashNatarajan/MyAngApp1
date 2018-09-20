181e1e26df51579ebdb380db7801033fcbaef075
PrakashNatarajan <prakashntrjn@gmail.com>
2018-09-20 18:51:24 +0530

    Modified for Shape with multidimensional array.

--- src/app/shape/shape.service.ts
+++ src/app/shape/shape.service.ts
@@ -5,7 +5,6 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Shape } from './shape';
-import { MessageService } from '../message.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -14,11 +13,11 @@ const httpOptions = {
 @Injectable({ providedIn: 'root' })
 export class ShapeService {
 
-  private shapesUrl = 'http://localhost:3000/shapes';  // URL to web api
+  private shapesUrl = 'http://localhost:12345/shapes';  // URL to web api
 
   constructor(
     private http: HttpClient,
-    private messageService: MessageService) { }
+  ) { }
 
   /** GET shapes from the server */
   getShapes (): Observable<Shape[]> {
@@ -29,72 +28,7 @@ export class ShapeService {
       );
   }
 
-  /** GET shape by id. Return `undefined` when id not found */
-  getShapeNo404<Data>(id: number): Observable<Shape> {
-    const url = `${this.shapesUrl}/?id=${id}`;
-    return this.http.get<Shape[]>(url)
-      .pipe(
-        map(shapes => shapes[0]), // returns a {0|1} element array
-        tap(h => {
-          const outcome = h ? `fetched` : `did not find`;
-          this.log(`${outcome} shape id=${id}`);
-        }),
-        catchError(this.handleError<Shape>(`getshape id=${id}`))
-      );
-  }
-
-  /** GET shape by id. Will 404 if id not found */
-  getShape(id: number): Observable<Shape> {
-    const url = `${this.shapesUrl}/${id}`;
-    return this.http.get<Shape>(url).pipe(
-      tap(_ => this.log(`fetched shape id=${id}`)),
-      catchError(this.handleError<Shape>(`getshape id=${id}`))
-    );
-  }
-
-  /* GET shapes whose title contains search term */
-  searchshapes(term: string): Observable<Shape[]> {
-    if (!term.trim()) {
-      // if not search term, return empty shape array.
-      return of([]);
-    }
-    return this.http.get<Shape[]>(`${this.shapesUrl}/?title=${term}`).pipe(
-      tap(_ => this.log(`found shapes matching "${term}"`)),
-      catchError(this.handleError<Shape[]>('searchshapes', []))
-    );
-  }
-
-  //////// Save methods //////////
-
-  /** POST: add a new shape to the server */
-  addShape (shape: Shape): Observable<Shape> {
-    return this.http.post<Shape>(this.shapesUrl, shape, httpOptions).pipe(
-      tap((shape: Shape) => this.log(`added shape w/ id=${shape.id}`)),
-      catchError(this.handleError<Shape>('addshape'))
-    );
-  }
-
-  /** DELETE: delete the shape from the server */
-  deleteshape (shape: Shape | number): Observable<Shape> {
-    const id = typeof shape === 'number' ? shape : shape.id;
-    const url = `${this.shapesUrl}/${id}`;
-
-    return this.http.delete<Shape>(url, httpOptions).pipe(
-      tap(_ => this.log(`deleted shape id=${id}`)),
-      catchError(this.handleError<Shape>('deleteshape'))
-    );
-  }
-
-  /** PUT: update the shape on the server */
-  updateshape (shape: Shape): Observable<any> {
-    const id = shape.id;
-    const url = `${this.shapesUrl}/${id}`;
-    return this.http.put(url, shape, httpOptions).pipe(
-      tap(_ => this.log(`updated shape id=${id}`)),
-      catchError(this.handleError<any>('updateshape'))
-    );
-  }
-
+  
   /**
    * Handle Http operation that failed.
    * Let the app continue.
@@ -115,8 +49,24 @@ export class ShapeService {
     };
   }
 
-  /** Log a shapeService message with the MessageService */
+  /** Log a shapeService message with the ShapeService */
   private log(message: string) {
-    this.messageService.add(`shapeService: ${message}`);
+    //this.messageService.add(`articleService: ${message}`);
+    console.log(message)
+  }  
+
+  private getMultidimensionalArray(arr: Shape[], rows: number, cols: number): Shape[][] {
+    var multimatrix = [];
+    if (rows * cols === arr.length) {
+      for(var i = 0; i < arr.length; i+= cols) {
+        multimatrix.push(arr.slice(i, cols + i));
+      }
+    }
+    return multimatrix
   }
+
+/*var a = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+getMultidimensionalArray(a, 3, 3);
+*/
+  
 }
--- src/app/shape/shape.ts
+++ src/app/shape/shape.ts
@@ -1,4 +1,4 @@
 export class Shape {
-   constructor(public id: number, public name: string, public sp_type: string) { 
+   constructor(public shape_id: number, public shape_name: string, public color_code: string, public user_name: string, public last_updated_at: string) { 
    }
 }
